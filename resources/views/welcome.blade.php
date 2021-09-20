216dcbe628d8c481ca065787761cc8c6af6704c0
charles mbuthia <charles.mbuthia19@gmail.com>
2021-09-20 15:28:42 +0300

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset(css/app.css)}}">
+    <title>Laravel Mpesa</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row mt-5">
+            <div class="col-sm-8 mx-auto">
+                <div class="card">
+                    <div class="card-header">
+                        Obtain access token
+                    </div>
+                    <div class="card-body">
+                        <button class="btn btn-primary">
+                            Request Access Token
+                        </button>
+                    </div>
+                </div>
+                <div class="card mt-5">
+                    <div class="card-header">
+                        Register Urls
+                    </div>
+                    <div class="card-body">
+                        <button class="btn btn-primary">Register Urls</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
